5afb9945baf399af2e33005b0e7ba9028ae361e9
Archie <archiewood@outlook.com>
2020-11-05 01:31:21 +0000

    made flask app

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import pandas as pd
 import json
+from flask import Flask, render_template
 
 
 # In[10]:
@@ -65,4 +66,50 @@ final_df
 
 
 final_df.to_csv("data.csv",mode='a',header=False)
+
+
+# In[15]:
+
+
+app = Flask(__name__)
+
+
+@app.route("/") 
+def home(): 
+	df = plot_data()
+	return render_template('index.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+
+# In[ ]:
+
+
+import plotly.express as px
+
+
+# In[14]:
+
+
+def plot_data():
+    df_to_plot=pd.read_csv("data.csv")
+    df_to_plot=df_to_plot[(df_to_plot['description.runnerName']=='Joe Biden')|(df_to_plot['description.runnerName']=='Donald Trump')]
+    pivot = df_to_plot.pivot_table(index='lastMatchTime', columns='description.runnerName' ,values ='state.lastPriceTraded')
+    return pivot
+
+
+# In[3]:
+
+
+
+
+
+# In[9]:
+
+
+
+
+
+# In[12]:
+
+
+
 
